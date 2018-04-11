757138dddc9f556ef4a237cdbc06c0bcca7e67cf
githubfc <profchene@gmail.com>
2018-04-11 09:31:18 +0200

    creation blog

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'BLog WNY' });
+});
+
+
+
+
 module.exports = router;
