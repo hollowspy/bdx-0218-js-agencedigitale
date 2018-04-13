3ef21fa876bb26b72f27d092d00d60f558aef3ae
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-04-13 20:42:55 +0200

    MAJ route SQL emploi.js

--- routes/emploi.js
+++ routes/emploi.js
@@ -5,10 +5,42 @@ var connection = require('../controlBDD/bdd.js')
 /* GET recruteur page */
 router.get('/', function(req, res, next) {
     const listEmploi = [];
-    connection.query('SELECT * from missions')
 
-    res.render('page_emploi', { title: 'Page Emploi' });
-});
 
+    // Do the query to get data.
+    connection.query('SELECT * FROM missions', function(err, rows, fields) {
+            if (err) {
+                res.status(500).json({
+                    "status_code": 500,
+                    "status_message": "internal server error"
+                });
+            } else {
+                // Loop check on each row
+                console.log('requete SQL ok');
+                for (var i = 0; i < rows.length; i++) {
+
+                    // Create an object to save current row's data
+                    var emploi = {
+                        'name_poste': rows[i].nom_poste,
+                        'duree': rows[i].duree,
+                        'ville': rows[i].localisation,
+                        'descri_poste': rows[i].desc_poste
+                    }
+                    // Add object into array
+                    listEmploi.push(emploi);
+                }
+                console.log(listEmploi);
+
+                // Render index.pug page using array
+                res.render('page_emploi', {
+                    "listEmploi": listEmploi
+                });
+            }
+        });
+
+        // Close the MySQL connection
+        connection.end();
+
+    });
 
 module.exports = router;
