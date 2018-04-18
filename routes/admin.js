121e517008eaac7a72affa5bc4929a8d78d3649a
Fanny Gaudin <mauriceleptitpoissonrouge@hotmail.fr>
2018-04-18 16:16:42 +0200

    new base de données et nouveau appel

--- routes/admin.js
+++ routes/admin.js
@@ -2,55 +2,35 @@ var express = require('express');
 var router = express.Router();
 const connection = require('../controllers/config');
 
-// let sql = "SELECT * FROM bar; SELECT * FROM missions";
+let sql = "SELECT * FROM bar; SELECT * FROM missions";
 /* GET administration page */
 router.get('/', function(req, res, next) {
 
-  let barList = [];
-  connection.query("SELECT * FROM bar", function(err, rows, fields) {
+  // let barList = [];
+  connection.query(sql, function(err, rows, fields) {
 	  	if (err) {
 	  		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
 	  	} else {
-        // console.log(rows);
+        // console.log(rows[0]);
+        let bar = rows[0];
+        let missions = rows[1];
 	  		// Loop check on each row
-	  		for (var i = 0; i < rows.length; i++) {
-
-	  			// Create an object to save current row's data
-		  		var bar = {
-		  			'name':rows[i].name,
-		  			'numbers':rows[i].numbers,
-		  			'picto':rows[i].picto
-		  		}
-		  		// Add object into array
-		  		barList.push(bar);
+	  		// for (var i = 0; i < rows.length; i++) {
+        //
+	  		// 	// Create an object to save current row's data
+		  	// 	var bar = {
+		  	// 		'name':rows[i].name,
+		  	// 		'numbers':rows[i].numbers,
+		  	// 		'picto':rows[i].picto
+		  	// 	}
+		  	// 	// Add object into array
+		  	// 	barList.push(bar);
+        // Render index.pug page using array
+        res.render('admin', { bodyClass:'admin', bar, missions});
 	  	}
 
-	  	// Render index.pug page using array
-      res.render('admin', { bodyClass:'admin', 'barList': barList});
-	  	}
-    });
-    // let workList = [];
-    // connection.query(sql, function(err, rows, fields) {
-  	//   	if (err) {
-  	//   		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-  	//   	} else {
-  	//   		// Loop check on each row
-  	//   		for (var i = 0; i < rows.length; i++) {
-    //
-  	//   			// Create an object to save current row's data
-  	// 	  		var work = {
-  	// 	  			'nom':rows[i].nom_poste,
-  	// 	  			'recruteur':rows[i].recruteur,
-  	// 	  			'duree':rows[i].duree
-  	// 	  		}
-  	// 	  		// Add object into array
-  	// 	  		workList.push(work);
-  	//   	}
-    //
-  	//   	// Render index.pug page using array
-    //     res.render('admin', {'workList': workList});
-  	//   	}
-    //   });
+
+	  });
 }); // fin de l'appel au get
 
 module.exports = router;
