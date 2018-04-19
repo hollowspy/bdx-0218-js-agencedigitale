a4c138283189df7c32cbf79c1823910ba6a6a72f
Grégory Petit <grpetit@gmail.com>
2018-04-19 17:32:36 +0200

    update index with fun bar and collab from SQL database

--- routes/index.js
+++ routes/index.js
@@ -2,36 +2,31 @@ var express = require('express');
 var router = express.Router();
 const connection = require('../controllers/config');
 
+const querySQL = 'SELECT * FROM bar WHERE type = ? ; SELECT * FROM collab';
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  let barList = [];
-  connection.query('SELECT * FROM bar WHERE type = ?', ['fun'], function(err, rows, fields) {
-	  	if (err) {
-	  		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-	  	} else {
-	  		// Loop check on each row
-	  		for (var i = 0; i < rows.length; i++) {
+	let barList = [];
+	connection.query(querySQL, ['fun'], function (err, rows, fields) {
+		if (err) {
+			res.status(500).json({ "status_code": 500, "status_message": "internal server error" });
+		} else {
+			// Loop check on each row
+			console.log(rows);
+			var fun = rows[0];
+			var collab = rows[1];
+			// Render index.pug page using array
+			res.render('index', { 'barList': fun, bodyClass: 'developpeur', page: 'developpeur' });
+		}
 
-	  			// Create an object to save current row's data
-		  		var bar = {
-		  			'name':rows[i].name,
-		  			'numbers':rows[i].numbers,
-		  			'picto':rows[i].picto
-		  		}
-		  		// Add object into array
-		  		barList.push(bar);
-	  	}
-
-	  	// Render index.pug page using array
-	  	res.render('index', {'barList': barList, bodyClass: 'developpeur', page: 'developpeur'});
-	  	}
-    });
+	});
 });
 
 /* GET blog page. */
-router.get('/blog', function(req, res, next) {
-  res.render('blog', { title: 'Blog WNY' });
+router.get('/blog', function (req, res, next) {
+	res.render('blog', { title: 'Blog WNY' });
 });
 
 module.exports = router;
