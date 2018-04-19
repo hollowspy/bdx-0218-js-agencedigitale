411d2907e1bc7527a062234c60ad1a69263dc5dd
Fanny Gaudin <mauriceleptitpoissonrouge@hotmail.fr>
2018-04-19 16:22:22 +0200

    mise en place de la partie administation

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,29 @@
 var express = require('express');
 var router = express.Router();
 const connection = require('../controllers/config');
-
+let sql = 'SELECT * FROM bar WHERE type = ?';
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   let barList = [];
-  connection.query('SELECT * FROM bar WHERE type = ?', ['fun'], function(err, rows, fields) {
+  connection.query(sql, ['fun'], function(err, rows, fields) {
 	  	if (err) {
 	  		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
 	  	} else {
 	  		// Loop check on each row
-	  		for (var i = 0; i < rows.length; i++) {
-
-	  			// Create an object to save current row's data
-		  		var bar = {
-		  			'name':rows[i].name,
-		  			'numbers':rows[i].numbers,
-		  			'picto':rows[i].picto
-		  		}
-		  		// Add object into array
-		  		barList.push(bar);
-	  	}
-
-	  	// Render index.pug page using array
-	  	res.render('index', {'barList': barList, bodyClass: 'developpeur', page: 'developpeur'});
+        let fun = rows;
+	  		// for (var i = 0; i < rows.length; i++) {
+        //
+	  		// 	// Create an object to save current row's data
+		  	// 	var bar = {
+		  	// 		'name':rows[i].name,
+		  	// 		'numbers':rows[i].numbers,
+		  	// 		'picto':rows[i].picto
+		  	// 	}
+		  	// 	// Add object into array
+		  	// 	barList.push(bar);
+        // Render index.pug page using array
+  	  	res.render('index', {bodyClass: 'developpeur', page: 'developpeur', fun});
 	  	}
     });
 });
