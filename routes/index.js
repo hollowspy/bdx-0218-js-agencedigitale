ebb48c4eeeca41b9ae816304c28aa6abe767cd9d
Thibaut C <thibaut.cointet@gmail.com>
2018-04-20 10:59:38 +0200

    Ajout page concept

--- routes/index.js
+++ routes/index.js
@@ -5,36 +5,6 @@ const connection = require('../controllers/config');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-<<<<<<< HEAD
-    let barList = [];
-    connection.query('SELECT * FROM bar WHERE type = ?', ['fun'], function(err, rows, fields) {
-        if (err) {
-            res.status(500).json({
-                "status_code": 500,
-                "status_message": "internal server error"
-            });
-        } else {
-            // Loop check on each row
-            for (var i = 0; i < rows.length; i++) {
-
-                // Create an object to save current row's data
-                var bar = {
-                        'name': rows[i].name,
-                        'numbers': rows[i].numbers,
-                        'picto': rows[i].picto
-                    }
-                    // Add object into array
-                barList.push(bar);
-            }
-
-            // Render index.pug page using array
-            res.render('index', {
-                'barList': barList,
-                bodyClass: 'developpeur',
-                page: 'developpeur'
-            });
-        }
-=======
   let barList = [];
   connection.query('SELECT * FROM bar WHERE type = ?', ['fun'], function(err, rows, fields) {
 	  	if (err) {
@@ -56,7 +26,6 @@ router.get('/', function(req, res, next) {
 	  	// Render index.pug page using array
 	  	res.render('index', {'barList': barList, bodyClass: 'developpeur', page: 'developpeur'});
 	  	}
->>>>>>> df772e5fbd2fc240911879b5ec1236a253924fea
     });
 });
 
