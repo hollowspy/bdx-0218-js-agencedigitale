80356fb0236b628b3d188ebac5931634af7c15a1
fredchene <profchene@gmail.com>
2018-04-25 17:42:19 +0200

    ajout landing articl blog

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 const connection = require('../controllers/config');
 const querySQL = 'SELECT * FROM blog ORDER BY date_parution DESC LIMIT 3';
+const queryGetBlogId = 'SELECT * FROM blog WHERE id=?';
 
 /* GET blog page. */
 router.get('/', (req, res, next) => {
@@ -14,11 +15,23 @@ router.get('/', (req, res, next) => {
 		} else {
 			// Loop check on each row
 			console.log(rows);
-			var blogJson = rows;
-    res.render('blog', { 'blogs': blogJson, title: 'Blog WNY', moment });
+    res.render('blog', { 'blogs': rows, title: 'Blog WNY', moment });
     }
   });
 });
 
+
+router.get('/:id', (req, res, next) => {
+
+  connection.query(queryGetBlogId, req.params.id, function (err, blogs, fields) {
+    if (err) {
+			res.status(500).json({ "status_code": 500, "status_message": "internal server error" });
+		} else {
+			// Loop check on each row
+			console.log(blogs.length);
+    res.render('full_article', { article : blogs[0], moment });
+    }
+  });
+});
 
 module.exports = router;
