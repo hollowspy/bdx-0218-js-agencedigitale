435bea1c97a0dbd43e428cf6fc4bc5384e91ecdc
coraliebrtl <coralie.brt@live.fr>
2018-04-27 11:46:33 +0200

    finalllllll

--- routes/recruteur.js
+++ routes/recruteur.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var ejs = require('ejs');
+var pg = require('pg');
+var bodyParser = require('body-parser');
 var mysql = require('mysql');
 const connection = require('../controllers/config');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
 let sql = 'SELECT * FROM bar WHERE type = "serious"; SELECT * FROM collab ;SELECT * FROM contact';
 
 /* GET recruteur page */
 router.get('/', function(req, res, next) {
-	
+
   	  connection.query(sql, function(err, rows, fields) {
 	  	if (err) {
 	  		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
@@ -19,6 +25,15 @@ router.get('/', function(req, res, next) {
 	  	res.render('recruteur', {bodyClass: 'recruteur', contact, bar, collab});
 	  	}
 	  	// Render index.pug page using array
+    });
+});
+
+router.post('/addEmploi', function(req, res) {
+  console.log('ok')
+	connection.query("INSERT INTO missions SET ?", req.body, function(err, result) {
+        if (err)
+            throw err;
+        res.redirect('/recruteur')
     });
 });
 
