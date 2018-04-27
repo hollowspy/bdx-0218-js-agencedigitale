2399fd1cd6bb54566bc96e65782ddf8f5d5d9b5a
fredchene <profchene@gmail.com>
2018-04-27 18:49:53 +0200

    style article id blog

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,6 +1,6 @@
 /* Scirpt de transformation de la Navbar  */
-$(document).ready(function() {
-  $(window).on("scroll", function() {
+$(document).ready(function () {
+  $(window).on("scroll", function () {
 
     if ($(window).scrollTop() >= 10) {
       $(".navbar").addClass("compressed");
@@ -11,7 +11,7 @@ $(document).ready(function() {
     }
 
 
-    if ($(window).scrollTop() >window.innerHeight) {
+    if ($(window).scrollTop() > window.innerHeight) {
 
       $(".navbar-brand").css("display", "block");
     } else {
@@ -21,25 +21,25 @@ $(document).ready(function() {
 
   });
 
-   // ajout de slick
-   $(".slick").slick({
+  // ajout de slick
+  $(".slick").slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 981,
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 981,
         // breakpoint: 992,
         settings: {
           slidesToShow: 2,
@@ -54,57 +54,57 @@ $(document).ready(function() {
           slidesToScroll: 1
         }
       }
-      ]
-    });
+    ]
+  });
 
 
 
-   /* Script Animation BLOG */
+  /* Script Animation BLOG */
 
-// Create the array with the text you want to write
+  // Create the array with the text you want to write
 
 
   /* Script de la serious bar lors du scroll */
 
   var a = 0;
-  $(window).scroll(function() {
+  $(window).scroll(function () {
 
     var oTop = $('#counter').offset().top - window.innerHeight;
     if (a == 0 && $(window).scrollTop() > oTop) {
-      $('.counter-value').each(function() {
+      $('.counter-value').each(function () {
         var $this = $(this),
-        countTo = $this.attr('data-count');
+          countTo = $this.attr('data-count');
         $({
           countNum: $this.text()
         }).animate({
           countNum: countTo
         },
 
-        {
+          {
 
-          duration: 1000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
+            duration: 1000,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
 
-        });
+          });
       });
       a = 1;
     }
 
   });
 
-// Url image offre emploi
+  // Url image offre emploi
 
-$(".search logo_entreprise").each(function(){
+  $(".search logo_entreprise").each(function () {
     let url = $(this).attr('src');
     console.log(url);
-})
+  })
 
 
 
@@ -112,14 +112,14 @@ $(".search logo_entreprise").each(function(){
   // PARTIE ADMINISTRATION
   // //////////////////////
 
-  $(".links").click(function(){
+  $(".links").click(function () {
     $(this).parent().addClass('active').siblings().removeClass('active');
     $(".divs:visible").hide();
-    $("#"+$(this).attr("data-showdiv")).show();
+    $("#" + $(this).attr("data-showdiv")).show();
   });
 
   // remplir la modal dynamiquement
-  $('.modifier').click(function(e){
+  $('.modifier').click(function (e) {
     e.preventDefault;
     let idModal = $(this).data('target');
     idModal = idModal.substring(1);
@@ -128,30 +128,45 @@ $(".search logo_entreprise").each(function(){
     let idTable = $(this).data('rowId');
     $('.modal-id form .form-group').remove();
 
-    $(this).parent().prevAll('.d-table-cell').each(function(){
+    $(this).parent().prevAll('.d-table-cell').each(function () {
       let label = $(this).data('label');
       let Txt = $(this).text();
       let name = $(this).data('name');
-      let input = '<div class="form-group"><label>'+label+'</label><input class="form-control" value="'+Txt+'" name="'+name+'" type="text"></input></div>';
-      $('.modal-id form').prepend(input);
+
+      if ($(this).data('label') === 'Contenu') {
+        let textArea = '<div class="form-group"><label>' +
+          label + '</label><textarea class="form-control" name="' + name + '">'+Txt+'</textarea></div>';
+        $('.modal-id form').prepend(textArea);
+      }else if($(this).data('type') === 'file'){
+        let upload = '<div class="form-group"><label>' +
+            label + '</label><input class="form-control" value="' + Txt +
+            '" name="' + name + '" type="file"></input></div>';
+        $('.modal-id form').prepend(upload);
+    }
+      else{
+        let input = '<div class="form-group"><label>' + label + '</label><input class="form-control" value="' + Txt + '" name="' + name + '" type="text"></input></div>';
+        $('.modal-id form').prepend(input);
+      }
+
     });
 
-    if ($(this).hasClass('bar')){
-      let action = '/admin/bar/'+idTable+'?_method=PUT';
+    if ($(this).hasClass('bar')) {
+      let action = '/admin/bar/' + idTable + '?_method=PUT';
       $('.modal-id form').attr('action', action);
-    } else if ($(this).hasClass('missions')){
-      let action = '/admin/missions/'+idTable+'?_method=PUT';
+    } else if ($(this).hasClass('missions')) {
+      let action = '/admin/missions/' + idTable + '?_method=PUT';
       $('.modal-id form').attr('action', action);
     } else if ($(this).hasClass('collab')) {
-      let action = '/admin/collab/'+idTable+'?_method=PUT';
+      let action = '/admin/collab/' + idTable + '?_method=PUT';
       $('.modal-id form').attr('action', action);
     } else if ($(this).hasClass('contact')) {
-      let action = '/admin/contact/'+idTable+'?_method=PUT';
+      let action = '/admin/contact/' + idTable + '?_method=PUT';
       $('.modal-id form').attr('action', action);
     } else if ($(this).hasClass('blog')) {
-      let action = '/admin/blog/'+idTable+'?_method=PUT';
+      let action = '/admin/blog/' + idTable + '?_method=PUT';
       $('.modal-id form').attr('action', action);
     }
+    tinymce.init({ selector:'textarea' });
 
     $(idModal).modal('show');
 
