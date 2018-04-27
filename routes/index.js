f9c0a6333626c15140c7fcceb35a4f78eff98058
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-04-27 18:32:40 +0200

    envoie nodemailer OK

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../controllers/config');
+const nodemailer = require("nodemailer");
+
 
 let sql = 'SELECT * FROM bar WHERE type = "fun"; SELECT * FROM missions WHERE valide = 1; SELECT * FROM contact';
 /* GET home page. */
@@ -29,6 +31,63 @@ router.get('/', function(req, res, next) {
         }
     });
 });
+
+
+//Route pour l'envoie des mails
+
+
+
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    host: 'smtp-relay.gmail.com',
+    auth: {
+        user: "julien.nied@gmail.com",
+        pass: "JargefoN1"
+    }
+});
+
+
+
+router.get('/addEmploi', function(req, res, next) {
+
+    connection.query(sql, function(err, rows, fields) {
+        if (err) {
+            res.status(500).json({
+                "status_code": 500,
+                "status_message": "internal server error"
+            });
+        } else {
+            console.log('youpi');
+            smtpTransport.sendMail({
+                from: "julien.nied@gmail.com", // Expediteur
+                to: "hollowspy@free.fr", // Destinataires
+                subject: "Nouvelle offre d'emploi !", // Sujet
+                text: "Nouvelle offre d'emploi !", // plaintext body
+                html: "Bonjour. Une nouvelle offre d'emploi vient d'être postée. Merci d'aller sur l'administration pour la valider ou non", // html body
+            })
+            // Loop check on each row
+            let fun = rows[0];
+            let emploi = rows[1];
+            let contact = rows[2];
+
+            res.render('confirm_emploi', {
+                bodyClass: 'developpeur',
+                page: 'developpeur',
+                fun,
+                emploi,
+                contact
+            });
+        }
+    });
+});
+
+
+
+
+
+
+
 
 
 module.exports = router;
